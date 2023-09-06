5860c1529c17c50a34c70537100cfaf4ff005109
wajid <wajidsiddiqi12@gmail.com>
2023-09-06 12:58:16 +0500

    added mint logic & withdraw func

--- backend/contracts/KingKat.sol
+++ backend/contracts/KingKat.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 error KingKat__TokenNonexistent();
+error KingKat__MintNotEnabled();
+error KingKat__NotEnoughMoneySent();
+error KingKat__WeSoldOut();
+error KingKat__WrongId();
 
 contract KingKat is ERC1155, Ownable, ERC1155Supply {
     uint256 private constant TOKEN_A = 1;
@@ -18,13 +22,19 @@ contract KingKat is ERC1155, Ownable, ERC1155Supply {
         ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
     {}
 
-    function changeMintState() public onlyOwner {
+    function changeMintState() external onlyOwner {
         if (s_mintState != 1) s_mintState = 1;
         else s_mintState = 2;
     }
 
-    function mint(uint256 id, uint256 amount) public payable {
-        _mint(msg.sender, id, amount, "");
+    function mint(uint256 id, uint256 quantity) public payable {
+        if (id > TOKEN_A) revert KingKat__WrongId();
+        if (s_mintState == 1) revert KingKat__MintNotEnabled();
+        if (msg.value != PRICE * quantity) revert KingKat__MintNotEnabled();
+        if (totalSupply(id) + quantity > MAX_SUPPLY)
+            revert KingKat__WeSoldOut();
+
+        _mint(msg.sender, id, quantity, "");
     }
 
     function uri(
@@ -38,6 +48,11 @@ contract KingKat is ERC1155, Ownable, ERC1155Supply {
             string(
                 abi.encodePacked(super.uri(id), Strings.toString(id), ".json")
             );
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 ammount = address(this).balance;
+        payable(msg.sender).transfer(ammount);
     }
 
     function mintBatch(
