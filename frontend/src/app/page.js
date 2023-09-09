67cb10c424688e57bfc58c85df39ceaa8a61f969
wajid <wajidsiddiqi12@gmail.com>
2023-09-09 17:14:14 +0500

    adding basic things

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
 import Image from "next/image";
-import "./styles/custom-font.css";
 import Footer from "./components/Footer";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import Header from "./components/Header";
@@ -12,9 +11,43 @@ import {
   StyledButton,
   TextWrapper,
   ImageContainer,
-  bgImageStyle,
+  ImageStyle,
+  Center,
+  LeftSide,
+  RightSide,
+  Container,
+  Box,
 } from "@/app/styles/styles.js";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Container>
+        <LeftSide>
+          <MainH1Title>King Kat</MainH1Title>
+          <ParaBig>
+            Lorem ipsum dolor sit amet. Qui pariatur illo et inventore dolor ut
+            facere voluptas non tempora praesentium quo dignissimos laborum qui
+            consequatur aliquam! Et ullam alias et similique sunt ad distinctio
+            repudiandae ut veniam atque cum odio nostrum qui soluta dicta. Est
+            culpa eligendi ut voluptas molestiae et natus mollitia ex fugiat
+            dolore eum magni necessitatibus.
+          </ParaBig>
+        </LeftSide>
+        <RightSide>
+          <Box>
+            <Image
+              src="/assets/nfts/1.jpg"
+              alt="NFT"
+              style={ImageStyle}
+              width={0}
+              height={0}
+              layout="responsive"
+            />
+          </Box>
+        </RightSide>
+      </Container>
+    </React.Fragment>
+  );
 }
